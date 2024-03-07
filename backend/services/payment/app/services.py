b56f1bd7a743de9f0696f6e57b24bd7bb05b8a96
jiayenbeh <jiayenbeh@gmail.com>
2024-03-07 16:16:23 +0800

    Added payment microservice

---
+++ backend/services/payment/app/services.py
@@ -0,0 +1,15 @@
+import requests  # Or the library you use for API calls
+from datetime import datetime, timedelta
+
+# def call_flight_inventory(payload):
+#     url = "http://flight_inventory:5000/flight/search"
+#     response = requests.post(url, json = payload)
+
+#     print(response)
+
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         raise Exception("Failed to retrieve flight details")  # Handle error appropriately
+
+# Other service functions for querying the inventory service 
