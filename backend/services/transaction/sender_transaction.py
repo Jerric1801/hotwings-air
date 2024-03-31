331390290a9c1b3db1432fe6fe03bd6d374e435f
jiayenbeh <jiayenbeh@gmail.com>
2024-03-31 14:59:27 +0800

    Added Transaction microservice and manage to consume massage from process payment

---
+++ backend/services/transaction/sender_transaction.py
@@ -0,0 +1,29 @@
+#THIS FILE IS A PLACEHOLDER FOR PAYMENT MICROSERVICE SENDING OVER TRANSACTION DETAILS WHEN A TRANSACTION OCCURS
+import pika
+import json
+
+# Establish connection 
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare the queue (Create a Queue)
+channel.queue_declare(queue='transaction_logs')
+
+# Send a message
+test_message = {
+    'type': 'p',
+    'user_id': '1123465.',
+    'payment_amt': "1002..50",
+    'loyalty_points': '1005',
+    'price_difference': '100.50.'
+
+    
+}
+channel.basic_publish(exchange='',
+                      routing_key='transaction_logs',
+                      body=json.dumps(test_message))
+
+print(" [x] Sent 'test_message'")
+
+# Close the connection
+connection.close()
