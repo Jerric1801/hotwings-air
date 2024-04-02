e39de6848d5ebf9e040e3ab98223eb7ad550d743
shawnwjh <wong.shawnjh@gmail.com>
2024-04-02 13:41:49 +0800

    customform done

--- backend/services/customform/index.js
+++ backend/services/customform/index.js
@@ -1,6 +1,9 @@
 require('./db/mongoose');
 const express = require("express");
+const bodyParser = require("body-parser");
 const customform = require('./models/customform');
+const Producer = require('./producer');
+const producer = new Producer();
 
 const app = express();
 
@@ -8,6 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(bodyParser.json("application/json"));
 
 app.post('/form', (req, res) => {
     const recommendations = new customform(req.body);
@@ -36,6 +40,11 @@ app.get('/form/:date&:flight', (req, res) => {
         res.status(500).send(error);
     })
 });
+
+app.post('/sendform', async(req, res, next) => {
+    await producer.publishMessage('notifications', req.body.email, req.body.notification_type, req.body.notification_message);
+    res.send();
+})
 
 app.listen(4545, () => {
     console.log("--- Custom Form Service is running ---")
